92b3dd0952849c55a0aa275fb26a71ebb8c3eaff
bulbulpaul <bulbulpaul0325@gmail.com>
2016-12-02 00:16:12 +0900

    Added decorator skeleton

--- websocket/flask_websocket.py
+++ websocket/flask_websocket.py
@@ -3,10 +3,41 @@
 from flask import request
 
 
-class WebSockets(object):
+class WebSocket(object):
 
-    def __init__(self, app=None):
+    def __init__(self, app=None, prefix='/ws'):
         self.app = app
+        self.init_app(app)
 
     def init_app(self, app):
+        app.add_url_rule(self.prefix, 'ws', self._handler)
+
+    def on_message(self, msg):
+        def decorator(fn):
+            pass
+            return f
+        return 
+
+    def on_open(self):
+        def decorator(fn):
+            pass
+            return f
+        return 
+
+    def on_close(self):
+        def decorator(fn):
+            pass
+            return f
+        return 
+
+    def on_error(self):
+        def decorator(fn):
+            pass
+            return f
+        return 
+
+    def send(self):
+        pass
+
+    def _handler(self):
         pass
